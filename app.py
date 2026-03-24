1a21dcb651602b337a7b0b90bba614b81b6696e9
abosdem <abosdem@outlook.sa>
2026-03-24 09:54:10 +0300

    app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import os
 
 app = Flask(__name__)
 
-BOT_TOKEN = "حط_توكن_البوت_هنا"
-CHAT_ID = "حط_ايدي_التليجرام_هنا"
+BOT_TOKEN = "8452344889:AAFkEzBOJ5RdWmXAQtxt8s42R_TUWPlrfFo"
+CHAT_ID = "912977673"
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -19,18 +19,15 @@ def send_telegram(message):
 
 def bot_loop():
     send_telegram("🚀 البوت اشتغل بنجاح")
-
     while True:
-        print("🔥 البوت شغال ويفحص السوق...")
+        print("🔥 البوت شغال ويفحص السوق...", flush=True)
         time.sleep(60)
-
-threading.Thread(target=bot_loop).start()
 
 @app.route("/")
 def home():
     return "Bot is running"
 
-# 🔥 هذا أهم سطر
 if __name__ == "__main__":
+    threading.Thread(target=bot_loop, daemon=True).start()
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
